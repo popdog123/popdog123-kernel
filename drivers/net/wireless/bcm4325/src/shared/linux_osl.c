70059711a5f1ede718fac7d5ee8c70161f19c0df
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-08-22 15:31:38 +0200

    Fix build errors

--- drivers/net/wireless/bcm4325/src/shared/linux_osl.c
+++ drivers/net/wireless/bcm4325/src/shared/linux_osl.c
@@ -156,12 +156,19 @@ osl_t *
 osl_attach(void *pdev, uint bustype, bool pkttag)
 {
 	osl_t *osh;
-	gfp_t flags;
 
-	flags = (in_atomic()) ? GFP_ATOMIC : GFP_KERNEL;
-	osh = kzalloc(sizeof(osl_t), flags);
+/* BEGIN: 0005533 mingi.sung@lge.com 2010-03-27 */
+/* MOD 0005533: [WLAN] Fixing WBT issues on Wi-Fi driver */
+/* WBT Fix TD# 248394, 248395 */
+	if(!(osh = kmalloc(sizeof(osl_t), GFP_ATOMIC))){
 	ASSERT(osh);
+		return NULL;
+	}
+/* END: 0005533 mingi.sung@lge.com 2010-03-27 */
 
+	bzero(osh, sizeof(osl_t));
+
+	
 	ASSERT(ABS(BCME_LAST) == (ARRAYSIZE(linuxbcmerrormap) - 1));
 
 	osh->magic = OS_HANDLE_MAGIC;
@@ -366,8 +373,7 @@ osl_pktfree_static(osl_t *osh, void *p, bool send)
 	
 	for (i = 0; i < MAX_STATIC_PKT_NUM*2; i++)
 	{
-		if ( (i < MAX_STATIC_PKT_NUM && p == bcm_static_skb->skb_4k[i]) ||
-		  (i >= MAX_STATIC_PKT_NUM && p == bcm_static_skb->skb_8k[i-MAX_STATIC_PKT_NUM]) )
+		if (p == bcm_static_skb->skb_4k[i])
 		{
 			down(&bcm_static_skb->osl_pkt_sem);
 			bcm_static_skb->pkt_use[i] = 0;
